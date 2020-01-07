96649eb379cd418464cf787d3c150cbc0ff607f3
Jérémy Zangla <zangla.jeremy@gmail.com>
2020-01-07 10:24:19 +0100

    Meilleur draw de la simulation et ajout de log :D

--- Code/src/main.cpp
+++ Code/src/main.cpp
@@ -1,7 +1,15 @@
 #include <random>
+#include <string>
+#include <fstream>
+#include <chrono>
+#include <thread>
 #include "System.hpp"
 #include "Entities/Agent/Harvester.hpp"
 #include "Utils/Map.hpp"
+
+enum DRAW_MODE {
+    MODE_LOG, MODE_DRAW, MODE_LOG_FILE, MODE_LOG_FILE_AND_DRAW
+};
 
 constexpr int SYS_W = 20;
 constexpr int SYS_H = 20;
@@ -10,8 +18,20 @@ std::mt19937 gen(256);
 std::uniform_int_distribution<> dis_x(0, SYS_W);
 std::uniform_int_distribution<> dis_y(0, SYS_H);
 
-int main()
+DRAW_MODE mode = MODE_LOG;
+std::fstream log_file;
+
+
+void getDrawMode(int, char **);
+void draw(System const &);
+
+int main(int argc, char ** argv)
 {
+    getDrawMode(argc, argv);
+
+    if (mode == MODE_LOG_FILE || mode == MODE_LOG_FILE_AND_DRAW)
+        log_file = std::fstream("output.log");
+
     System s(SYS_W, SYS_H);
 
     Base* base = new Base(Point(5, 5), 5);
@@ -22,29 +42,68 @@ int main()
     for (int i = 0; i < 20; i++)
     {
         s.update();
-        std::cout << s;
+        draw(s);
     }
     
+    if (mode == MODE_LOG_FILE || mode == MODE_LOG_FILE_AND_DRAW)
+        log_file.close();
+
     return 0;
 }
 
-void testGetDirection()
+void draw(System const & s)
 {
-    Map m(10, 10);
-
-    Point c(6, 6);
-    Point p(6, 0);
-
-    m[c] = 'B';
-    m[p] = 'p';
-
-    std::cout << m;
-    while (c != p)
+    switch (mode)
     {
-        p += m.getDirection(p, c);
-        m.clear();
-        m[c] = 'B';
-        m[p] = 'p';
-        std::cout << m;
+    case MODE_DRAW:
+        #if defined(_WIN32)
+        system("CLS");
+        #else
+        system("clear");
+        #endif 
+        std::cout << s;
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        break;
+    case MODE_LOG:
+        std::cout << s;
+        break;
+    case MODE_LOG_FILE:
+        log_file << s;
+        break;
+    case MODE_LOG_FILE_AND_DRAW:
+        log_file << s;
+        #if defined(_WIN32)
+        system("CLS");
+        #else
+        system("clear");
+        #endif
+        std::cout << s;
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        break;
+    }
+}
+
+void getDrawMode(int argc, char ** argv) 
+{
+    if (argc != 2)
+        mode = MODE_LOG;
+    else 
+    {
+        std::string arg = std::string(argv[1]);
+        if (arg == "--log")
+            mode = MODE_LOG;
+        else if (arg == "--draw")
+        {
+            mode = MODE_DRAW;
+        }
+        else if (arg == "--log-file")
+            mode = MODE_LOG_FILE;
+        else if (arg == "--log-draw")
+            mode = MODE_LOG_FILE_AND_DRAW;
+        else
+        {
+            std::cout << "Use only with : <--draw | --log | --log-file | --log-draw>" << std::endl;
+            exit(1);
+        }
     }
 }
